42e1fb13b4455a0f80a0dc78c86f93fca81c70d9
lefotrakrm121711@gmail.com <Aa121711!>
2023-02-15 20:55:25 +0300

    g

--- app/src/main/java/com/example/proj/MainActivity.java
+++ app/src/main/java/com/example/proj/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.proj;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -21,53 +23,50 @@ import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity  {
 
     //Экземпляры классов наших кнопок
-    Button redButton,greenButton,blueButton,whiteButton,onB,offB,noB;
-    int kol;
-    int[] im= new int[]{R.id.im1, R.id.im2, R.id.im3, R.id.im4};
+    Button redButton, greenButton, blueButton, whiteButton, onB, offB, noB,sendB;
+    int kol,onf;
+    int[] marks = new int[4];
+    int[] im = new int[]{R.id.im1, R.id.im2, R.id.im3, R.id.im4};
     //Сокет, с помощью которого мы будем отправлять данные на Arduino
     BluetoothSocket clientSocket;
-
+    OutputStream outStream;
     //Эта функция запускается автоматически при запуске приложения
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         redButton = findViewById(R.id.button4);
-        noB= findViewById(R.id.button9);
+        noB = findViewById(R.id.button9);
         greenButton = findViewById(R.id.button3);
         blueButton = findViewById(R.id.button5);
         whiteButton = findViewById(R.id.button6);
-        onB=findViewById(R.id.button);
-        offB=findViewById(R.id.button2);
+        onB = findViewById(R.id.button);
+        sendB=findViewById(R.id.button10);
+        offB = findViewById(R.id.button2);
+        onB.setOnClickListener(this::onClickB);
+        offB.setOnClickListener(this::onClickB);
         //Добавлем "слушатель нажатий" к кнопке
-        redButton.setOnClickListener(this);
-        greenButton.setOnClickListener(this);
-        blueButton.setOnClickListener(this);
-        whiteButton.setOnClickListener(this);
-        noB.setOnClickListener(this);
-        kol=0;
+        sendB.setOnClickListener(this::onFinal);
+        redButton.setOnClickListener(this::onClickB);
+        greenButton.setOnClickListener(this::onClickB);
+        blueButton.setOnClickListener(this::onClickB);
+        whiteButton.setOnClickListener(this::onClickB);
+        noB.setOnClickListener(this::onClickB);
+        kol = 0;
 
         String enableBT = BluetoothAdapter.ACTION_REQUEST_ENABLE;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+
         startActivityForResult(new Intent(enableBT), 0);
 
         //Мы хотим использовать тот bluetooth-адаптер, который задается по умолчанию
         BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 
         //Пытаемся проделать эти действия
-        try{
+        try {
             //Устройство с данным адресом - наш Bluetooth Bee
             //Адрес опредеяется следующим образом: установите соединение
             //между ПК и модулем (пин: 1234), а затем посмотрите в настройках
@@ -76,10 +75,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             //Инициируем соединение с устройством
             Method m = device.getClass().getMethod(
-                    "createRfcommSocket", new Class[] {int.class});
+                    "createRfcommSocket", new Class[]{int.class});
 
             clientSocket = (BluetoothSocket) m.invoke(device, 1);
             clientSocket.connect();
+            outStream = clientSocket.getOutputStream();
 
             //В случае появления любых ошибок, выводим в лог сообщение
         } catch (IOException e) {
@@ -102,96 +102,95 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
 
-
     //Как раз эта функция и будет вызываться
 
-    @Override
-    public void onClick(View v) {
+
+    public void onClickB(View v) {
 
         //Пытаемся послать данные
         //Получаем выходной поток для передачи данных
-        //OutputStream outStream = clientSocket.getOutputStream();
-        int[] marks = new int[4];
-        int onf=0;
-        if(v==redButton){
-            marks[kol]=1;
-            ImageView ql=findViewById(im[kol]);
+
+
+
+        onf = 0;
+        if (v == redButton) {
+            marks[kol] = 2;
+            ImageView ql = findViewById(im[kol]);
             ql.setImageResource(R.drawable.red);
-            if(kol<3) {
+            if (kol < 3) {
                 kol += 1;
-            }
-            else{
-                kol=1;
+            } else {
+                kol = 1;
             }
 
 
         }
-        if(v==greenButton){
-            marks[kol]=2;
-            ImageView ql=findViewById(im[kol]);
+        if (v == greenButton) {
+            marks[kol] = 3;
+            ImageView ql = findViewById(im[kol]);
             ql.setImageResource(R.drawable.green);
-            if(kol<3) {
+            if (kol < 3) {
                 kol += 1;
-            }
-            else{
-                kol=0;
+            } else {
+                kol = 0;
             }
 
         }
-        if(v==blueButton){
-            marks[kol]=3;
-            ImageView ql=findViewById(im[kol]);
+        if (v == blueButton) {
+            marks[kol] = 4;
+            ImageView ql = findViewById(im[kol]);
             ql.setImageResource(R.drawable.blue);
-            if(kol<3) {
+            if (kol < 3) {
                 kol += 1;
-            }
-            else{
-                kol=0;
+            } else {
+                kol = 0;
             }
 
         }
-        if(v==whiteButton){
-            marks[kol]=4;
-            ImageView ql=findViewById(im[kol]);
+        if (v == whiteButton) {
+            marks[kol] = 5;
+            ImageView ql = findViewById(im[kol]);
             ql.setImageResource(R.drawable.white);
-            if(kol<3) {
+            if (kol < 3) {
                 kol += 1;
-            }
-            else{
-                kol=0;
+            } else {
+                kol = 0;
             }
 
         }
-        if (v==noB){
-            marks[kol]=0;
-            ImageView ql=findViewById(im[kol]);
+        if (v == noB) {
+            marks[kol] = 6;
+            ImageView ql = findViewById(im[kol]);
             ql.setImageResource(R.drawable.black);
-            if(kol<3) {
+            if (kol < 3) {
                 kol += 1;
-            }
-            else{
-                kol=0;
+            } else {
+                kol = 0;
             }
 
         }
 
-        if(v==onB){
-            onf=1;
+        if (v == onB) {
+            onf = 1;
         }
-        if(v==offB){
-            onf=0;
+        if (v == offB) {
+            onf = 0;
         }
-        //outStream.write(onf);
-        //for(int i=0;i<3;i++){
-        //    outStream.write(marks[i]);
-        //    if(i<2){
-        //        outStream.write(5);
-        //    }
-        //    else{
-        //        outStream.write(6);
-        //    }
-        //}
+
 
     }
 
+    public void onFinal(View v) {
+        String send1=Integer.toString(onf);
+        for (int i = 0; i < 3; i++) {
+            send1+=Integer.toString(marks[i]);
+        }
+        Integer send2=Integer.parseInt(send1);
+        try {
+            outStream.write(send2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
